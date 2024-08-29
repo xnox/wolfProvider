c4d94609c80377d6e41c56f63a59d0a0b1afc256
Dimitri John Ledkov <dimitri.ledkov@surgut.co.uk>
2024-08-29 00:02:53 -0400

    simple benchmarks

--- scripts/build-wolfprovider.sh
+++ scripts/build-wolfprovider.sh
@@ -10,4 +10,7 @@ echo "Using openssl: $OPENSSL_TAG, wolfssl: $WOLFSSL_TAG"
 
 init_wolfprov
 
+$OPENSSL_INSTALL_DIR/bin/openssl speed -provider default sha256 rsa2048 ecdsap256
+$OPENSSL_INSTALL_DIR/bin/openssl speed -provider libwolfprov.so.0 -provider-path $WOLFPROV_PATH sha256 rsa2048 ecdsap256
+
 exit $?
